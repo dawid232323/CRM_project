241b51cff9262a86f83a426cb37cecc4250db066
Dawid Pylak <dawpylak@gmail.com>
2021-10-30 20:13:35 +0200

    started working on models

--- crm_rest_api_server/models.py
+++ crm_rest_api_server/models.py
@@ -1,19 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    login = models.CharField(max_length=20)
-    # password =
+# TODO add companies, trade note, contact person
 
 
 class Roles(models.Model):
     role_name = models.CharField(max_length=100)
-    
+
+
+class CmrUser(User):
+    # present fields: first_name, last_name, username
+    date_of_birth = models.DateField()
+    role_id = models.ForeignKey(Roles, on_delete=models.CASCADE)
+
 
 class Business(models.Model):
     business_name = models.CharField(max_length=100)
-
-
