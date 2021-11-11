664af3695dec72cedb5e52a33a83d5b77d4012f0
Dawid Pylak <dawpylak@gmail.com>
2021-11-11 15:12:41 +0100

    started working on logging in

---
+++ crm-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Test!</h1>
+        {/*<Link to='/login'>Login</Link>*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ crm-frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from "./components/loginPage";
+
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/home" element={<App/>}/>
+        </Routes>
+        {/*<App/>*/}
+    </BrowserRouter>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
